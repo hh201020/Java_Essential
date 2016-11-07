c9bda23a2b594ac2bbd7b90365833a69d1268721
Jerry Hu <hh201020@gmail.com>
2016-11-07 11:43:12 -0500

    Enumeration Classes

--- src/main/java/com/lynda/olivepress/Main.java
+++ src/main/java/com/lynda/olivepress/Main.java
@@ -3,16 +3,16 @@ package com.lynda.olivepress;
 import java.util.ArrayList;
 
 import com.lynda.olivepress.olives.OliveJar;
+import com.lynda.olivepress.olives.OliveName;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 
 		OliveJar jar = new OliveJar();
-		jar.addOlive("Kalamata", 0x000000);
-		jar.addOlive("Kalamata", 0x000000);
-		jar.addOlive("Kalamata", 0x000000);
-		jar.addOlive("Kalamata", 0x000000);
+		jar.addOlive(OliveName.KALAMATA, 0x000000);
+		jar.addOlive(OliveName.PICHOLINE, 0x000000);
+		jar.addOlive(OliveName.LIGURIO, 0x000000);
 		jar.reportOlives();
 	}
 	
--- src/main/java/com/lynda/olivepress/olives/OliveJar.java
+++ src/main/java/com/lynda/olivepress/olives/OliveJar.java
@@ -9,31 +9,24 @@ public class OliveJar {
 	{
 		System.out.println("initializing...");
 		olives = new ArrayList<>();
-		olives.add(new Olive("Golden", 0xDA9100));
+		olives.add(new Olive(OliveName.GOLDEN, 0xDA9100));
 	}
 	
 	public OliveJar() {
 		System.out.println("Constructor...");
 	}
 	
-	public OliveJar(int nOlives, String oliveName, long color) {
+	public OliveJar(int nOlives, OliveName oliveName, long color) {
 		for (int i = 1; i <= nOlives; i++) {
 			olives.add(new Olive(oliveName, color));
 		}
 	}
 
-	public void addOlive(String oliveName, long color) {
+	public void addOlive(OliveName oliveName, long color) {
 		olives.add(new Olive(oliveName, color));
 	}
 	
 	public void reportOlives() {
-	//		class JarLid {
-	//		public void open() {
-	//			System.out.println("Twist, twist, twist...");
-	//			System.out.println("Pop!");
-	//		}
-	//	}
-	//	new JarLid().open();
 		
 		new Object() {
 			public void open() {
@@ -43,34 +36,8 @@ public class OliveJar {
 		} .open();
 		
 		for (Olive o : olives) {
-			System.out.println("It's a " + o.oliveName + " olive!!");	
+			System.out.println("It's a " + o.oliveName + " olive!!");
 		}
 	}
-	
-	class Olive {
-		
-		public static final long BLACK = 0x000000;
-		
-		public String oliveName;
-		public long color = BLACK;
-		
-		public Olive() {
-		}
-
-		public Olive(String oliveName) {
-			this.oliveName = oliveName;
-		}
-		
-		public Olive(String oliveName, long color) {
-			this(oliveName);
-			this.color = color;
-		}
-		
-		public String toString() {
-			return "name: " + this.oliveName + ": " + "color: " +  this.color;
-		}
-
-	}
-
 	
 }
