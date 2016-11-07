0d599ccb88e09d522774ce99f77c49440a0ca60f
Jerry Hu <hh201020@gmail.com>
2016-11-07 11:22:08 -0500

    Member classes

--- src/main/java/com/lynda/olivepress/Main.java
+++ src/main/java/com/lynda/olivepress/Main.java
@@ -2,20 +2,18 @@ package com.lynda.olivepress;
 
 import java.util.ArrayList;
 
-import com.lynda.olivepress.olives.Olive;
 import com.lynda.olivepress.olives.OliveJar;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		ArrayList<Olive> oliveStatic = new OliveJar(10, "Kalamata", 0x000000).olives;
+	public static void main(String[] args) throws Exception {
 
-		for (Olive o:oliveStatic) {
-			System.out.println("It's a " + o.oliveName
-					+ " olive!");
-		}
-		
+		OliveJar jar = new OliveJar();
+		jar.addOlive("Kalamata", 0x000000);
+		jar.addOlive("Kalamata", 0x000000);
+		jar.addOlive("Kalamata", 0x000000);
+		jar.addOlive("Kalamata", 0x000000);
+		jar.reportOlives();
 	}
-
+	
 }
